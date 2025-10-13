1dbfd9cb184c1bc3be9fb4429826b176c37cd735
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-13 18:08:15 +0530

    feat(frontend): add Navbar component and integrate it into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import ThemeToggler from "./components/ThemeToggler";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   // Initialize from local storage or fallback to system / default
   return (
     <div>
-      <ThemeToggler />
+      <Navbar />
     </div>
   );
 };
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import ThemeToggler from "./ThemeToggler";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-primary shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-primary rounded-box z-1 mt-6 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Menu</a>
+            </li>
+            <li>
+              <a>Mobile App</a>
+            </li>
+            <li>
+              <a>Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">FoodieFly</a>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />{" "}
+          </svg>
+        </button>
+        <button className="btn btn-ghost btn-circle">
+          <div className="indicator">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-shopping-cart-icon lucide-shopping-cart"
+            >
+              <circle cx="8" cy="21" r="1" />
+              <circle cx="19" cy="21" r="1" />
+              <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+            </svg>
+          </div>
+        </button>
+        <ThemeToggler />
+        <button className="btn btn-outline">Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

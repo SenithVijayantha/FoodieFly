e4f59524a33c59d06e5d6f2f1f3dac387743ea9b
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-13 21:48:52 +0530

    feat(navbar): add sticky positioning to keep navbar visible on scroll

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-primary shadow-sm">
+    <div className="sticky top-0 z-10 navbar bg-primary shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">

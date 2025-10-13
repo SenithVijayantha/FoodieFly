25ad4edb7291cae7f6b93c36de7c14f8839af068
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-13 20:50:10 +0530

    chore: create assets.js to export assets from assets folder

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,32 @@
+export const menu_list = [
+  {
+    menu_name: "Salad",
+    menu_image:
+      "https://plus.unsplash.com/premium_photo-1676047258557-de72954cf17c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=739",
+  },
+  {
+    menu_name: "Beverages",
+    menu_image:
+      "https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=957",
+  },
+  {
+    menu_name: "Desserts",
+    menu_image:
+      "https://plus.unsplash.com/premium_photo-1716892888227-7051cb976c98?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=869",
+  },
+  {
+    menu_name: "Starters",
+    menu_image:
+      "https://images.unsplash.com/photo-1706650439799-d4a8894556b6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764",
+  },
+  {
+    menu_name: "Main Course",
+    menu_image:
+      "https://plus.unsplash.com/premium_photo-1663840345377-3813d196d5da?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687",
+  },
+  {
+    menu_name: "Sides",
+    menu_image:
+      "https://images.unsplash.com/photo-1598679253544-2c97992403ea?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687",
+  },
+];

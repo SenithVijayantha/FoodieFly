9305e48919121f4340e8907ef10065e555e4238a
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-17 22:42:38 +0530

    feat: setup routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import AddProductPage from "./pages/AddProductPage";
+import ListProductsPage from "./pages/ListProductsPage";
+import OrdersPage from "./pages/OrdersPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Sidebar />
+      <Routes>
+        <Route path="/add" element={<AddProductPage />} />
+        <Route path="/list" element={<ListProductsPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;

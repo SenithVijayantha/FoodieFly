49b2e0205731d646a5dbd0a3e9631ff2cc6995a3
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-17 15:34:40 +0530

    feat: add request validations and follow best practices

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -3,21 +3,50 @@ import fs from "fs";
 
 // add food items
 export const addFood = async (req, res) => {
-  let image_filename = `${req.file.filename}`;
-
-  const food = new foodModel({
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-    category: req.body.category,
-    image: image_filename,
-  });
-
   try {
+    // validate request body
+    if (
+      !req.body.name ||
+      !req.body.description ||
+      !req.body.price ||
+      !req.body.category ||
+      !req.file
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide all required fields",
+      });
+    }
+
+    const food = new foodModel({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      image: req.file.filename,
+    });
+
     await food.save();
-    res.status(201).json({ success: true, message: "Item added" });
+    res.status(201).json({ success: true, message: "Item added successfully" });
   } catch (error) {
     console.error(error);
-    res.json({ success: false, message: "Error " });
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+// get food list
+export const getFoodsList = async (req, res) => {
+  try {
+    const foods = await foodModel.find().lean(); // lean() for better performance
+    if (!foods || foods.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No foods found",
+      });
+    }
+    res.status(200).json({ success: true, data: foods });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };

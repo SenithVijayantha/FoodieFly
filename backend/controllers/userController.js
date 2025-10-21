dd6aa7060e25331ecffa49838b34758aa1b2cba2
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-21 12:21:35 +0530

    feat(auth): setup controller for user sign-in

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,42 @@ import userModel from "../models/userModel.js";
 import { setAuthCookie } from "../utils/jwt.js";
 
 // sing in
-export const signInUser = async (req, res) => {};
+export const signInUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User does not exists" });
+    }
+
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+
+    if (!isMatchPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect password",
+      });
+    }
+
+    const payload = { userId: user._id };
+    setAuthCookie(res, payload);
+
+    return res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+    });
+  } catch (error) {
+    console.error("sign in error", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error. Please try again later.",
+    });
+  }
+};
 
 // sign up
 export const signUpUser = async (req, res) => {
@@ -55,7 +90,7 @@ export const signUpUser = async (req, res) => {
     });
   } catch (error) {
     console.error("signup error", error.message);
-    res.status(500).json({
+    return res.status(500).json({
       success: false,
       message: "Internal server error. Please try again later.",
     });

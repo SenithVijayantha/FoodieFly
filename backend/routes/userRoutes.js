148b61a471670e12558f21351c9615473738e6ad
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-21 19:12:38 +0530

    feat(routes): add signout and auth status routes to userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 
-import { signInUser, signUpUser } from "../controllers/userController.js";
+import {
+  authenticateUser,
+  signInUser,
+  signOutUser,
+  signUpUser,
+} from "../controllers/userController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signUpUser);
 userRouter.post("/signin", signInUser);
+userRouter.post("/signout", signOutUser);
+userRouter.get("/auth/status",  authenticate,authenticateUser);
 
 export default userRouter;

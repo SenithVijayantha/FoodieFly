3a7882de5cf91bbdefabc7fb3ffdfcdd86dcfc75
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-22 11:05:54 +0530

    feat: Implements the controller function for  user cart item addition.
    - updating item quantity

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,7 +1,44 @@
 import userModel from "../models/userModel.js";
 
 // add items to user cart
-export const addToCart = async (req, res) => {};
+export const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId } = req.body;
+
+    console.log(itemId);
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing userId" });
+    } else if (!itemId) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing itemId" });
+    }
+
+    const userData = await userModel.findById(userId);
+    const cartData = userData.cartData || {};
+
+    console.log("Adding item:", itemId);
+
+    if (!cartData[itemId]) {
+      cartData[itemId] = 1;
+    } else {
+      cartData[itemId] += 1;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+
+    res.status(201).json({ success: true, message: "Added to cart" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while trying to add to the cart",
+    });
+  }
+};
 
 // remove items from user cart
 export const removeFromCart = async (req, res) => {};

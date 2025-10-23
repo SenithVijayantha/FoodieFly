8e2420d107729c22e5295404aa2b70f3e0d3d739
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-23 23:57:02 +0530

    feat(order): implement placeOrder function for processing user orders with Stripe integration

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,56 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// place an order
+export const placeOrder = async (req, res) => {
+  const frontend_url = process.env.FRONTEND_URL;
+
+  try {
+    const newOrder = new orderModel({
+      userId: req.user.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
+
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.user.userId, { cartData: {} });
+
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: 2 * 100,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    return res.status(200).json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.error("Error placing order:", error);
+    return res.status(500).json({ success: false, message: "Failed to place order" });
+  }
+};

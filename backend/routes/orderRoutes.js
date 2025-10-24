fc1824dbe5320e411d53dd57296a4b12cb0d9b86
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-24 21:05:53 +0530

    feat(order): update order routes to include userOrders and listAllOrders, and ensure authentication for verifyOrder

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import {
+  listAllOrders,
+  placeOrder,
+  userOrders,
+  verifyOrder,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authenticate, placeOrder);
-orderRouter.post("/verify", verifyOrder);
+orderRouter.post("/verify", authenticate, verifyOrder);
+orderRouter.post("/myorders", authenticate, userOrders);
+orderRouter.post("/myorders", authenticate, userOrders);
+orderRouter.get("/list", authenticate, listAllOrders);
 
 export default orderRouter;

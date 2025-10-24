55ca46e2d8a023e21d64bd07e8678360f8dd2481
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-10-24 11:48:08 +0530

    feat(order): Add order submission logic to PlaceOrderPage.jsx

--- frontend/src/pages/PlaceOrderPage.jsx
+++ frontend/src/pages/PlaceOrderPage.jsx
@@ -1,52 +1,162 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 import { StoreContext } from "../context/StoreContext";
 
 const PlaceOrderPage = () => {
-  const { getTotalCartItemsPrice, deliveryFee, cartItemsCount } =
-    useContext(StoreContext);
+  const {
+    getTotalCartItemsPrice,
+    deliveryFee,
+    cartItemsCount,
+    foodList,
+    cartItems,
+    url,
+  } = useContext(StoreContext);
+
+  const [deliveryInfo, setDeliveryInfo] = useState({});
+
+  const handleDeliveryInfo = (e) => {
+    setDeliveryInfo((deliveryInfo) => ({
+      ...deliveryInfo,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let orderItems = [];
+    foodList.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+
+    let orderData = {
+      address: deliveryInfo,
+      items: orderItems,
+      amount: getTotalCartItemsPrice() + 2,
+    };
+
+    try {
+      let response = await axios.post(
+        `${url}/api/order/place`,
+        orderData,
+        {
+          withCredentials: true,
+        }
+      );
+      if (response.data.success) {
+        const { session_url } = response.data;
+        window.location.replace(session_url);
+      } else {
+        toast.error("Error");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    // console.log(orderItems);
+    // console.log(deliveryInfo);
+    return;
+  };
 
   return (
     <div>
-      <form className="grid lg:grid-cols-2 items-start place-items-center my-14">
+      <form
+        onSubmit={handleSubmit}
+        className="grid lg:grid-cols-2 items-start place-items-center my-14"
+      >
         <div>
           <h3 className="text-2xl font-semibold p-4">Delivery Information</h3>
           <fieldset className="grid gap-4 fieldset w-xs p-4">
             <div className="flex flex-col sm:flex-row gap-2">
               <input
+                required
+                name="firstName"
                 type="text"
                 className="input w-60"
                 placeholder="First Name"
+                value={deliveryInfo.firstName || ""}
+                onChange={handleDeliveryInfo}
               />
               <input
+                required
+                name="lastName"
                 type="text"
                 className="input w-60"
                 placeholder="Last Name"
+                value={deliveryInfo.lastName || ""}
+                onChange={handleDeliveryInfo}
               />
             </div>
             <input
+              required
+              name="email"
               type="email"
               className="input w-80 sm:w-122"
               placeholder="Email address"
+              value={deliveryInfo.email || ""}
+              onChange={handleDeliveryInfo}
             />
-            <input type="email" className="input  w-80 sm:w-122" placeholder="Street" />
+            <input
+              required
+              name="street"
+              type="text"
+              className="input  w-80 sm:w-122"
+              placeholder="Street"
+              value={deliveryInfo.street || ""}
+              onChange={handleDeliveryInfo}
+            />
             <div className="flex flex-col sm:flex-row gap-2">
-              <input type="text" className="input w-60" placeholder="City" />
               <input
+                required
+                name="city"
+                type="text"
+                className="input w-60"
+                placeholder="City"
+                value={deliveryInfo.city || ""}
+                onChange={handleDeliveryInfo}
+              />
+              <input
+                required
+                name="state"
                 type="text"
                 className="input w-60"
                 placeholder="State/Province"
+                value={deliveryInfo.state || ""}
+                onChange={handleDeliveryInfo}
               />
             </div>
             <div className="flex flex-col sm:flex-row gap-2">
               <input
+                required
+                name="zipcode"
                 type="text"
                 className="input w-60"
                 placeholder="Zip code"
+                value={deliveryInfo.zipcode || ""}
+                onChange={handleDeliveryInfo}
               />
-              <input type="text" className="input w-60" placeholder="Country" />
+              <input
+                required
+                name="country"
+                type="text"
+                className="input w-60"
+                placeholder="Country"
+                value={deliveryInfo.country || ""}
+                onChange={handleDeliveryInfo}
+              />
             </div>
-            <input type="email" className="input  w-80 sm:w-122" placeholder="Phone" />
+            <input
+              required
+              name="phone"
+              type="tel"
+              className="input  w-80 sm:w-122"
+              placeholder="Phone"
+              value={deliveryInfo.phone || ""}
+              onChange={handleDeliveryInfo}
+            />
           </fieldset>
         </div>
         <div>
@@ -82,7 +192,6 @@ const PlaceOrderPage = () => {
             </table>
             <button
               className="btn btn-primary btn-sm"
-              onClick={() => navigate("/order")}
               disabled={cartItemsCount === 0}
               type="submit"
             >
